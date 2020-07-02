9a215278043634249616863006228cd595dec528
Dante Astudillo <daac99@gmail.com>
2020-07-01 22:19:29 -0500

    change the variables to final and remove the autowired annotation is better practice use constructor injection

--- src/main/java/com/mx/daac/service/AuthService.java
+++ src/main/java/com/mx/daac/service/AuthService.java
@@ -3,6 +3,7 @@ package com.mx.daac.service;
 import com.mx.daac.dto.RegisterRequest;
 import com.mx.daac.model.Users;
 import com.mx.daac.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,13 +12,11 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.Instant;
 
 @Service
+@AllArgsConstructor
 public class AuthService {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     @Transactional
     public void signUp(RegisterRequest registerRequest){
