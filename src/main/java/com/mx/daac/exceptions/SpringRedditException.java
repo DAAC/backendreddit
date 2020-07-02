f680eccb607e323c4079ddffcfd02390e5746717
Dante Astudillo <daac99@gmail.com>
2020-07-02 15:38:19 -0500

    new exception handle for email address

---
+++ src/main/java/com/mx/daac/exceptions/SpringRedditException.java
@@ -0,0 +1,7 @@
+package com.mx.daac.exceptions;
+
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String exMessage) {
+        super(exMessage);
+    }
+}
