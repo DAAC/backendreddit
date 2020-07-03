29c8df28c4107c30d40250f950b2a0e85c5e2c79
Dante Astudillo <daac99@gmail.com>
2020-07-03 11:36:28 -0500

    added the configureGlobal with authmanagerbuilder get user details for SecurityConfig

---
+++ src/main/java/com/mx/daac/service/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package com.mx.daac.service;
+
+import com.mx.daac.model.Users;
+import com.mx.daac.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Users> usersOptional = userRepository.findByUsername(username);
+        Users users = usersOptional.orElseThrow(() -> new UsernameNotFoundException("the username does not exist "+ username));
+        return new User(users.getUsername(), users.getPassword(), users.isEnabled(),
+                true, true, true,
+                getAuthorities("USER"));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return Collections.singletonList(new SimpleGrantedAuthority(role));
+    }
+}
