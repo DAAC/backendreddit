0f531526408bd33daceb042c894031e9d5a39aed
Dante Astudillo <daac99@gmail.com>
2020-07-15 00:36:46 -0500

    allow access for post api

--- src/main/java/com/mx/daac/config/SecurityConfig.java
+++ src/main/java/com/mx/daac/config/SecurityConfig.java
@@ -31,6 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers(HttpMethod.GET, "/api/subreddit")
                 .permitAll()
+                .antMatchers(HttpMethod.GET,"/api/posts")
+                .permitAll()
                 .anyRequest()
                 .authenticated();
         httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
