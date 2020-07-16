931bb9c4e26634475aa55524732bae487db82f62
Dante Astudillo <daac99@gmail.com>
2020-07-16 17:06:01 -0500

    find vote by post and user

--- src/main/java/com/mx/daac/repository/VoteRepository.java
+++ src/main/java/com/mx/daac/repository/VoteRepository.java
@@ -1,9 +1,14 @@
 package com.mx.daac.repository;
 
+import com.mx.daac.model.Post;
+import com.mx.daac.model.Users;
 import com.mx.daac.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findTopByPostAndUserOrderByVoteIdDesc(Post post, Users currentUser);
 }
