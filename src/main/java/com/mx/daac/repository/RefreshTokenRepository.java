2b88b452f2c9b6b5e3a897520d230ac36e49461f
Dante Astudillo <daac99@gmail.com>
2020-07-16 23:51:20 -0500

    refresh token for login

---
+++ src/main/java/com/mx/daac/repository/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package com.mx.daac.repository;
+
+import com.mx.daac.model.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByToken(String token);
+}
