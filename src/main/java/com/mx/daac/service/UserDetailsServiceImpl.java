d088784e7810943b4d876fbc4e0ec9648f6ee1a7
Dante Astudillo <daac99@gmail.com>
2020-07-21 18:47:15 -0500

    added package for userdetails class

--- src/main/java/com/mx/daac/service/UserDetailsServiceImpl.java
+++ src/main/java/com/mx/daac/service/UserDetailsServiceImpl.java
@@ -5,7 +5,6 @@ import com.mx.daac.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,26 +12,31 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Optional;
+
+import static java.util.Collections.singletonList;
 
 @Service
 @AllArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
-
     private final UserRepository userRepository;
 
     @Override
     @Transactional(readOnly = true)
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Users> usersOptional = userRepository.findByUsername(username);
-        Users users = usersOptional.orElseThrow(() -> new UsernameNotFoundException("the username does not exist "+ username));
-        return new User(users.getUsername(), users.getPassword(), users.isEnabled(),
-                true, true, true,
-                getAuthorities("USER"));
+    public UserDetails loadUserByUsername(String username) {
+        Optional<Users> userOptional = userRepository.findByUsername(username);
+        Users users = userOptional
+                .orElseThrow(() -> new UsernameNotFoundException("No user " +
+                        "Found with username : " + username));
+
+        return new org.springframework.security
+                .core.userdetails.User(users.getUsername(), users.getPassword(),
+                users.isEnabled(), true, true,
+                true, getAuthorities("USER"));
     }
 
     private Collection<? extends GrantedAuthority> getAuthorities(String role) {
-        return Collections.singletonList(new SimpleGrantedAuthority(role));
+        return singletonList(new SimpleGrantedAuthority(role));
     }
 }
+
